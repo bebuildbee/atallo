93bc33ced6a83999515fd9d962dc48c5ef575b39
bebuildbee <brooskandar@gmail.com>
2026-08-06 10:42:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const backToTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 500){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
